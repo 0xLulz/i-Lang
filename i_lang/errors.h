9c3f86b91692daf7d9adde4a016d6205e7aaeb8a
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2021-12-30 16:57:26 -0500

    Create errors.h

---
+++ i_lang/errors.h
@@ -0,0 +1,25 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+typedef struct Errors {
+    char        **Errors[255];
+    char        **Error_Files[255];
+    int         Error_Lines[255];
+    int         Error_Count[255];
+    void*       (*add_error)(struct Error *e, char *err_file, int err_line, char *err);
+    void*       (*die)(char *msg);
+} Error;
+
+void __adderr(Error *e, char *err_file, int err_line, char *err) {
+
+}
+
+void err_n_die(char *msg) {
+    printf("%s", msg);
+    exit(0);
+}
+
+Error Build_Error(Error *e) {
+    e->add_error = __adderr;
+    e->die = err_n_die;
+}
+#endif
