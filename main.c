afe565fc9cfde9a080cd1b4d13edb78cf5d68a57
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-01-12 19:25:22 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,17 @@
 #include "i_lang/main.h"
 
 int main(int argc, char **argv[]) {
+    iLang i; Error e;
+    Build_iLang(&i); Build_Error(&e);
+    print("Starting up....", 1);
+    if(argc < 1) e.die("[x] Error, Invalid Argument");
 
+    for(int i = 0; i <= strlen(argv); i++) {
+        if(argv[i] == NULL) continue;
+        if(strstr(argv[i], ".i")) {
+            print(argv[i], 1);
+            return 0;
+        }
+    }
+    i.operate(&i, "test.i");
 }
